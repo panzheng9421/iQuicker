be17b0d6b0f18b4dc8b17634a4146489d8bc2c35
yichao qin <qinyc@yichaodemacbook-pro.local>
2017-10-20 15:28:30 +0800

    初始化项目

---
+++ iQuicker/iQuicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iQuicker
+//
+//  Created by yichao qin on 2017/10/20.
+//  Copyright © 2017年 iQuicker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
